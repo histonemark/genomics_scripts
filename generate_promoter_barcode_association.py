f7f376829193d9ed5de856541c1405e9a9fa621c
histonemark <corralesmarc@gmail.com>
2018-04-06 11:31:47 +0200

    Scripts generated during my phD

--- generate_promoter_barcode_association.py
+++ generate_promoter_barcode_association.py
@@ -1,1 +1,8 @@
-generate_promoter_barcode_association.py
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Standard library packages.
+import sys
+
+# Read a library sequencing experiment directory and associate
+# the barcodes with the promoters.
